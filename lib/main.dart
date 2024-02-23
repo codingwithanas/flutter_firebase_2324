18990e1be562f74e22c5b189b1a5abb706fdd61f
Anas Assadek <anasassadek@ceroca.cat>
2024-02-23 20:24:14 +0100

    Primers passos firebase escrits

--- lib/main.dart
+++ lib/main.dart
@@ -16,3 +16,13 @@ class MainApp extends StatelessWidget {
       );
   }
 }
+
+/*
+1) Tenir el Node.js instal-lat.
+2) npm install -g firebase-tools
+3) Fer login a Firebase: firebase login
+  - Si dona error de l'arxiu firebase.ps1, enviar-li el nom (o esborrar-lo
+  i tornar a fer login).
+  - Si tornem a fer firebase login, ens diu en quin compte estem.
+  - Si vulguéssim canviar el compte, fem firebase logout.
+*/
