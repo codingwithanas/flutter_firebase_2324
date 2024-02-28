6ea14ca7ae51d6a65644084d86d55a1814c2e779
Anas Assadek <anasassadek@ceroca.cat>
2024-02-28 20:26:47 +0100

    UI Registre

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase_2324/firebase_options.dart';
 import 'package:flutter_firebase_2324/pagines/pagina_login.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_firebase_2324/pagines/pagina_registre.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MainApp());
 }
 
@@ -16,7 +20,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: PaginaLogin(),
+      home: PaginaRegistre(),
     );
   }
 }
---
+++ lib/pagines/pagina_registre.dart
@@ -0,0 +1,153 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_firebase_2324/components/boto_auth.dart';
+import 'package:flutter_firebase_2324/components/textfield_auth.dart';
+
+class PaginaRegistre extends StatefulWidget {
+  const PaginaRegistre({super.key});
+
+  @override
+  State<PaginaRegistre> createState() => _PaginaRegistreState();
+}
+
+class _PaginaRegistreState extends State<PaginaRegistre> {
+  final TextEditingController controllerEmail = TextEditingController();
+  final TextEditingController controllerPassword = TextEditingController();
+    final TextEditingController controllerConfirmarPassword = TextEditingController();
+
+  void ferRegistre() {
+    
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 250, 183, 159),
+      body: Center(
+        child: SingleChildScrollView(
+          child: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // Logo.
+                const Icon(
+                  Icons.fireplace,
+                  size: 120,
+                  color: Color.fromARGB(255, 255, 240, 218),
+                ),
+
+                const SizedBox(
+                  height: 25,
+                ),
+
+                // Frase.
+                const Text(
+                  "Crea un compte nou",
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 255, 240, 218),
+                    fontSize: 18,
+                  ),
+                ),
+
+                const SizedBox(
+                  height: 25,
+                ),
+
+                // Text Divisori.
+                const Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 25,
+                  ),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Divider(
+                          thickness: 1,
+                          color: Color.fromARGB(255, 255, 240, 218),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 4,
+                        ),
+                        child: Text(
+                          "Registra't",
+                          style: TextStyle(
+                            color: Color.fromARGB(255, 255, 240, 218),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Divider(
+                          thickness: 1,
+                          color: Color.fromARGB(255, 255, 240, 218),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+
+                const SizedBox(
+                  height: 25,
+                ),
+
+                // TextField Email.
+                TextFieldAuth(
+                  controller: controllerEmail,
+                  obscureText: false,
+                  hintText: "Email",
+                ),
+
+                const SizedBox(
+                  height: 25,
+                ),
+
+                // TextField Password.
+                TextFieldAuth(
+                  controller: controllerPassword,
+                  obscureText: false,
+                  hintText: "Contraseña",
+                ),
+
+                const SizedBox(
+                  height: 25,
+                ),
+
+                // No estás registrat/da?
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      const Text("Ja ets membre?"),
+                      const SizedBox(
+                        width: 5,
+                      ),
+                      GestureDetector(
+                        onTap: () {},
+                        child: const Text(
+                          "Fes login",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Color.fromARGB(255, 40, 71, 97),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 25,
+                ),
+                // Botó Login.
+                BotoAuth(
+                  text: "Registra't",
+                  onTap: ferRegistre,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
