86a29683a99d9de6d4feb8ae88aee18b17cd479b
Anas Assadek <anasassadek@ceroca.cat>
2024-03-13 20:22:49 +0100

    Guardant dades a Firestore

--- lib/auth/servei_auth.dart
+++ lib/auth/servei_auth.dart
@@ -1,8 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class ServeiAuth {
   final FirebaseAuth _auth = FirebaseAuth.instance;
-
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   // fer login
   Future<UserCredential> loginAmbEmailIPassword(String email, password) async {
     try {
@@ -19,6 +20,12 @@ class ServeiAuth {
       UserCredential credencialUsuari = await _auth.createUserWithEmailAndPassword(
           email: email, password: password);
 
+
+  _firestore.collection("Usuaris").doc(credencialUsuari.user!.uid).set({
+    "uid": credencialUsuari.user!.uid,
+    "email": email,
+    "parametreX": "Hola",
+    });
       return credencialUsuari;
     } on FirebaseAuthException catch (e) {
       throw Exception(e.code);
